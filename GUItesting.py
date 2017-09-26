465c43815c5e53b9c29763d8b9cc26b21b2b09af
JulioBarahona <julio.fbm96@gmail.com>
2017-09-26 14:28:39 -0600

    GUI

---
+++ GUItesting.py
@@ -0,0 +1,14 @@
+from easygui import *
+import sys
+from Main import *
+
+#Ventana Inicial
+msgbox("                                   Proyecto #1\n"
+       "Integrantes\n"
+       "       Julio Barahona       141206\n"
+       "       Josue Jacobs         15041\n"
+       "       Jorgue Suchite       15293\n"
+       "       Gerson Samayoa       15337\n"
+       "       Kevin Sanchez?       ??????",'Metodos Numericos','Continuar')
+
+
